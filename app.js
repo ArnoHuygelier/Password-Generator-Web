ae9e21aa2576db9ffe3ea33c037e3947ca63f6fd
arno huygelier <arno2003.huygelier1@gmail.com>
2025-09-19 19:01:44 +0200

    added js for the basic logic to gen and pass

---
+++ app.js
@@ -0,0 +1,48 @@
+const lengthSlider = document.getElementById('lengthSlider');
+const lengthOutput = document.getElementById('lengthOutput');
+const generateBtn = document.getElementById('generateBtn');
+const generatedPassword = document.getElementById('generatedPassword');
+const copyBtn = document.getElementById('copyBtn');
+const strengthFill = document.getElementById('strengthFill');
+
+const uppercaseCheckbox = document.getElementById('uppercase');
+const lowercaseCheckbox = document.getElementById('lowercase');
+const numbersCheckbox = document.getElementById('numbers');
+const symbolsCheckbox = document.getElementById('symbols');
+
+//Connect slider value with lengthoutput
+
+lengthSlider.addEventListener('input', () =>{
+    lengthOutput.value = lengthSlider.value;
+})
+
+//Characters for passgen
+
+const UPPERCASE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LOWERCASE_CHARS = "abcdefghijklmnopqrstuvwxyz";
+const NUMBER_CHARS = "0123456789";
+const SYMBOL_CHARS = "!@#$%^&*()-_=+[]{};:,.<>?/";
+
+function genPassword() {
+    let length = parseInt(lengthSlider.value);
+    let charset = "";
+
+    if(uppercaseCheckbox.checked) charset += UPPERCASE_CHARS;
+    if(lowercaseCheckbox.checked) charset += LOWERCASE_CHARS;
+    if(numbersCheckbox.checked) charset += NUMBER_CHARS;
+    if(symbolsCheckbox.checked) charset += SYMBOL_CHARS;
+
+    if (charset === "") {
+        alert("Select at least one character type!");
+        return;
+    }
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += charset[Math.floor(Math.random() * charset.length)];
+    }
+
+    generatedPassword.value = password;
+}
+
+generateBtn.addEventListener('click', genPassword);
